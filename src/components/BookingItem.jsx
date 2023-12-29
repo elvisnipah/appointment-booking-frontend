2565bb7e9f2e75640b37533bad2fa74464d6d2db
Elvis <elvisnipah@gmail.com>
2023-12-29 19:21:49 +0000

    improve booking admin display

--- src/components/BookingItem.jsx
+++ src/components/BookingItem.jsx
@@ -1,6 +1,5 @@
 import { useDispatch } from "react-redux";
 import { acceptBooking, declineBooking } from "../reducers/bookingReducer";
-import { useState } from "react";
 
 const BookingItem = ({ item }) => {
   const dispatch = useDispatch();
@@ -13,20 +12,41 @@ const BookingItem = ({ item }) => {
     dispatch(declineBooking(item.id, { ...item, status: "declined" }));
   };
 
+  const itemStatus = () => {
+    switch (item.status) {
+      case "confirmed":
+        return "Accepted";
+      case "declined":
+        return "Declined";
+      case "waiting":
+        return "Pending";
+    }
+  };
+
+  const itemStatusStyle = () => {
+    switch (itemStatus()) {
+      case "Accepted":
+        return "p-2 inline-flex rounded-lg font-bold h-8 text-center text-sm bg-green-500 text-white";
+      case "Declined":
+        return "p-2 inline-flex rounded-lg font-bold h-8 text-center text-sm bg-red-500 text-white";
+      case "Pending":
+        return "p-2 inline-flex rounded-lg font-bold h-8 text-center text-sm bg-gray-500 text-white";
+    }
+  };
+
   return (
-    <div className="flex flex-col border-2 border-primary p-4 ">
-      {item.status === "confirmed" ? (
-        <div>confirmed</div>
-      ) : (
-        <p>not confirmed</p>
-      )}
-      <p>
-        Name: {item.firstName} {item.lastName}
-      </p>
+    <div className="flex flex-col border-2 border-primary p-4">
+      {/* <p className="self-end">hello</p> */}
+      <div className="flex justify-between">
+        <p>
+          {item.firstName} {item.lastName}
+        </p>
+        <p className={itemStatusStyle()}>{itemStatus()}</p>
+      </div>
       <p>{item.email}</p>
       <p>{item.phoneNumber}</p>
-      <p>Time: {new Date(item.appointmentTime).toUTCString()}</p>
-      <div className="flex gap-4">
+      <p>{new Date(item.appointmentTime).toUTCString()}</p>
+      <div className="flex gap-4 mt-4">
         <button className="btn btn-sm btn-success" onClick={confirm}>
           Confirm
         </button>
